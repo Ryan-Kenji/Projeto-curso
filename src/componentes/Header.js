bffa1ccdd450a37d43d9aca49c4cb2226e2869ee
Ryan-Kenji <ryan.nishimura.melo@escola.pr.gov.br>
2026-05-05 12:34:13 -0300

    Add files via upload

---
+++ src/componentes/Header.js
@@ -0,0 +1,22 @@
+function Header() {
+
+    return (
+        <header>
+            <div className="topo">
+                <h1>Curso Técnico em Desenvolvimento de Sistema</h1>
+            </div>
+            <nav className="headerMenu">
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre o Curso</li>
+                    <li>Disciplinas Curriclares</li>
+                    <li>Disciplinas Técnica</li>
+                    <li>Equipe</li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+
+}
+export default Header;
